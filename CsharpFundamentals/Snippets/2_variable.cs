8127c63ccd3b42b17e7e1d430e9c8cea2d2e47de
CableBK <dev.witchapat@gmail.com>
2025-02-04 19:51:47 +0700

    Update variable

---
+++ CsharpFundamentals/Snippets/2_variable.cs
@@ -0,0 +1,8 @@
+/*
+string Name = "Cable";
+int Age = 1;
+float Score = 1.9F;
+
+Console.WriteLine($"My name is {Name} and {Age} year old");
+Console.WriteLine("My score is " + Score + " point");
+*/
--- CsharpFundamentals/obj/Debug/net8.0/CsharpFundamentals.AssemblyInfo.cs
+++ CsharpFundamentals/obj/Debug/net8.0/CsharpFundamentals.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsharpFundamentals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+621a4c3e46e19be853dd9f0b34e7ead63f3d941c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a7e79e035b7b0d821e60960cd8472ce3507942")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsharpFundamentals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsharpFundamentals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
